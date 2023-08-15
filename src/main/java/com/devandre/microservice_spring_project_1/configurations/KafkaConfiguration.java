38e1d8dc05503be6d4486fe7a8089d3a0337b413
André <devandrecontact@gmail.com>
2023-08-15 13:45:54 -0300

    insert kafka

---
+++ src/main/java/com/devandre/microservice_spring_project_1/configurations/KafkaConfiguration.java
@@ -0,0 +1,48 @@
+package com.devandre.microservice_spring_project_1.configurations;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Value(value = "{spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        var configs = new HashMap<String, Object>();
+
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<String, Object>(producerFactory());
+    }
+}
--- src/main/java/com/devandre/microservice_spring_project_1/services/GeneratedTicketService.java
+++ src/main/java/com/devandre/microservice_spring_project_1/services/GeneratedTicketService.java
@@ -1,23 +1,45 @@
 package com.devandre.microservice_spring_project_1.services;
 
+import com.devandre.microservice_spring_project_1.dtos.KafkaDTO;
 import com.devandre.microservice_spring_project_1.dtos.TicketDTO;
 
 import io.github.benas.randombeans.EnhancedRandomBuilder;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeneratedTicketService {
 
+    public KafkaTemplate<String, Object> kafkaTemplate;
+
+    public GeneratedTicketService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Scheduled(fixedRate = 5000)
-    public void run() {
+    public void execute() {
         var enhancedRandomBuilder = EnhancedRandomBuilder.aNewEnhancedRandomBuilder();
-        var newTicketGenerated = enhancedRandomBuilder.build().nextObject(TicketDTO.class);
+        var newTicketGenerated = enhancedRandomBuilder.build()
+                                                      .nextObject(TicketDTO.class);
 
         var logger = LoggerFactory.getLogger(GeneratedTicketService.class);
 
-        logger.info(newTicketGenerated.toString());
+        var newMessageKafka = new KafkaDTO();
+
+        newMessageKafka.setTicketDTO(newTicketGenerated);
+
+        var response = kafkaTemplate.send(String.valueOf(enhancedRandomBuilder.build()
+                                                                              .nextInt()), newMessageKafka);
+
+        response.whenComplete((message, ex) -> {
+            if (ex == null) {
+                logger.info(newTicketGenerated.toString());
+            } else {
+                logger.warn("Deu ruim");
+            }
+        });
     }
 }
